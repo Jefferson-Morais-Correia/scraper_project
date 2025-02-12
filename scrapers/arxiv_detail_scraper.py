890a8c5ea0548b40cb4e8cde7e864fd6b4710389
Jefferson-Morais-Correia <jeffersondemoraiscorreia@gmail.com>
2025-02-11 22:06:53 -0300

    resolvendo bugs

--- scrapers/arxiv_detail_scraper.py
+++ scrapers/arxiv_detail_scraper.py
@@ -12,18 +12,22 @@ class ArxivDetailScraper(BaseScraper):
     def scrape(self):
         paper_url = f"{self.BASE_URL}/html/{self.paper_id}"
         response = self.session.get(paper_url, verify=False)
-        if response.status_code == 200:
 
-            soup = BeautifulSoup(response.content, "html.parser")
-            title = soup.find(class_="ltx_title").text.strip()
-            abstract = soup.find(class_="ltx_abstract").text.strip()
-            bibliography = soup.find(class_="ltx_bibliography").text.strip()
-
-            return {"Title": title, "Abstract": abstract, "References": bibliography}
-        
-        else: 
-    
+        if response.status_code != 200:
             print(f"Erro: Não foi possível obter detalhes do paper ID {self.paper_id}. Código de status: {response.status_code}")
-            return None  # Ou outro valor indicando falha
+            return None  # Retorna antes de chegar no BeautifulSoup
+
+        soup = BeautifulSoup(response.content, "html.parser")
+        
+        # Se chegou aqui, significa que response.status_code == 200
+        title_tag = soup.find(class_="ltx_title")
+        abstract_tag = soup.find(class_="ltx_abstract")
+        bibliography_tag = soup.find(class_="ltx_bibliography")
+
+        title = title_tag.text.strip() if title_tag else "Título não encontrado"
+        abstract = abstract_tag.text.strip() if abstract_tag else "Resumo não encontrado"
+        bibliography = bibliography_tag.text.strip() if bibliography_tag else "Bibliografia não encontrada"
+
+        return {"Title": title, "Abstract": abstract, "References": bibliography}
 
 
