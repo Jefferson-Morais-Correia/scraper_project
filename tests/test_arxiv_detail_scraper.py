5f833744ed6dea4933db929dbac6e8f67ad98727
Jefferson-Morais-Correia <jeffersondemoraiscorreia@gmail.com>
2025-02-11 21:42:25 -0300

    resolvendo bug

--- tests/test_arxiv_detail_scraper.py
+++ tests/test_arxiv_detail_scraper.py
@@ -11,7 +11,7 @@ def test_get_details(detail_scraper):
     """
     Testa se o método get_details retorna um dicionário com os detalhes do artigo.
     """
-    details = detail_scraper.scrape("2301.00001")
+    details = detail_scraper.scrape()
     assert isinstance(details, dict), "O retorno deve ser um dicionário."
     assert "Title" in details, "O dicionário deve conter a chave 'Title'."
     assert "Abstract" in details, "O dicionário deve conter a chave 'Abstract'."
