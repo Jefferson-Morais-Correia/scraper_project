cee74c8970c3c5aa005792737a58825e27650d67
Jefferson-Morais-Correia <jeffersondemoraiscorreia@gmail.com>
2025-02-11 22:01:11 -0300

    resolvendo bugs

--- scrapers/arxiv_detail_scraper.py
+++ scrapers/arxiv_detail_scraper.py
@@ -12,16 +12,18 @@ class ArxivDetailScraper(BaseScraper):
     def scrape(self):
         paper_url = f"{self.BASE_URL}/html/{self.paper_id}"
         response = self.session.get(paper_url, verify=False)
-        if response.status_code != 200:
+        if response.status_code == 200:
+
+            soup = BeautifulSoup(response.content, "html.parser")
+            title = soup.find(class_="ltx_title").text.strip()
+            abstract = soup.find(class_="ltx_abstract").text.strip()
+            bibliography = soup.find(class_="ltx_bibliography").text.strip()
+
+            return {"Title": title, "Abstract": abstract, "References": bibliography}
+        
+        else: 
+    
             print(f"Erro: Não foi possível obter detalhes do paper ID {self.paper_id}. Código de status: {response.status_code}")
             return None  # Ou outro valor indicando falha
-
-        soup = BeautifulSoup(response.content, "html.parser")
-        title = soup.find(class_="ltx_title").text.strip()
-        abstract = soup.find(class_="ltx_abstract").text.strip()
-        bibliography = soup.find(class_="ltx_bibliography").text.strip()
-
-        return {"Title": title, "Abstract": abstract, "References": bibliography}
-
 
 
